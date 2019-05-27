2b3ba5f9f0317d56e019ebae08677f581cde1fab
Joshua JL Mitchell <jl@jeniuslogic.com>
2019-05-27 16:51:50 -0400

    Using Segues

--- appSwoosh/appSwoosh/ViewController.swift
+++ appSwoosh/appSwoosh/ViewController.swift
@@ -16,7 +16,13 @@ class ViewController: UIViewController {
         
         
     }
-
-
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
 }
 
