6ecc083a552fb5e97937b95920f3d86db9829699
Joshua JL Mitchell <jl@jeniuslogic.com>
2019-06-12 14:15:49 -0400

    added new comments to viewContoller

--- appSwoosh/appSwoosh/Controller/WelcomeVC.swift
+++ appSwoosh/appSwoosh/Controller/WelcomeVC.swift
@@ -14,6 +14,8 @@ class WelcomeVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        /*swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        bgImg.frame = view.frame */ // This is the old way to place and center your subViews
         
     }
     @IBOutlet weak var swoosh: UIImageView!
